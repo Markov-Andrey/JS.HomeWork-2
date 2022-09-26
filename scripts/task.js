55d2c5b33affa02b65ce64b26cb7de70aca7c712
Markov Andrey <andremarkov@icloud.com>
2022-09-26 10:07:02 +0300

    task 2 complete and css-style changed

--- scripts/task.js
+++ scripts/task.js
@@ -1,12 +1,20 @@
+// TASK 1
 let name = prompt ('Ваше имя'),
-age = prompt ('Ваш возраст'), 
+age = +prompt ('Ваш возраст'), 
 city = prompt ('Город проживания'), 
 phone = prompt ('Ваш номер телефона'), 
 email = prompt ('Ваша электронная почта'), 
 company = prompt ('Место работы');
-document.write
-	('Меня зовут ' + name + '. Мне ' 
-	+ age + ' лет. Я проживаю в городе ' 
+document.write (
+	'Меня зовут ' + name + '. Мне ' 
+	+ age + ' лет.<br>Я проживаю в городе ' 
 	+ city + ' и работаю в компании ' 
-	+ company + '. Мои контактные данные: ' 
-	+ phone + ' и ' + email);
+	+ company + '.<br>Мои контактные данные: ' 
+	+ '<br>' + phone + '<br>' + email
+);
+// TASK 2
+let year = 2022 - age;
+document.write (
+	'<br>' + name + ' родился в ' 
+	+ year + ' году'
+);
