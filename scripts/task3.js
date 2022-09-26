248c462ef9136894e41625e58e792dfda3220e88
Markov Andrey <andremarkov@icloud.com>
2022-09-26 11:11:20 +0300

    task 3 complete

---
+++ scripts/task3.js
@@ -0,0 +1,13 @@
+// TASK 3
+let number = prompt ('Введите 6-значное число');
+console.log (number.substr(0,3))
+if (
+	+number.substr(0,1) + +number.substr(1,1) + +number.substr(2,1) == 
+	+number.substr(3,1) + +number.substr(4,1) + +number.substr(5,1)
+	) {
+		document.write (number.substr(0,3) + number.substr(3,3) + '<br>')
+		document.write ('Да, сумма первых 3-х чисел равна сумме других 3-х чисел')
+	} else {
+		document.write (number.substr(0,3) + number.substr(3,3) + '<br>')
+		document.write ('Нет, сумма первых 3-х чисел не равна сумме других 3-х чисел')
+	};
