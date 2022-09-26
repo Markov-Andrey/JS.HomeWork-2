3ec28f54246398e1982372bd2d96404d73e5eda0
Markov Andrey <andremarkov@icloud.com>
2022-09-26 09:23:42 +0300

    Task 1 complete

---
+++ scripts/task.js
@@ -0,0 +1,12 @@
+let name = prompt ('Ваше имя'),
+age = prompt ('Ваш возраст'), 
+city = prompt ('Город проживания'), 
+phone = prompt ('Ваш номер телефона'), 
+email = prompt ('Ваша электронная почта'), 
+company = prompt ('Место работы');
+document.write
+	('Меня зовут ' + name + '. Мне ' 
+	+ age + ' лет. Я проживаю в городе ' 
+	+ city + ' и работаю в компании ' 
+	+ company + '. Мои контактные данные: ' 
+	+ phone + ' и ' + email);
